33e5cb571942052038bc3a52a605662d05e2aecc
Leedabord <gwforumlakers@gmail.com>
2021-03-14 13:20:44 -0700

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,67 +1,10 @@
-import * as React from "react";
-import { render } from "react-dom";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd
-} from "@react-firebase/auth";
-import { firebaseConfig } from "./base";
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
 
-export const App = () => {
-  return (
-    <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
-      <div>
-        <button
-          onClick={() => {
-            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(googleAuthProvider);
-          }}
-        >
-          Sign In with Google
-        </button>
-        <button
-          data-testid="signin-anon"
-          onClick={() => {
-            firebase.auth().signInAnonymously();
-          }}
-        >
-          Sign In Anonymously
-        </button>
-        <button
-          onClick={() => {
-            firebase.auth().signOut();
-          }}
-        >
-          Sign Out
-        </button>
-        <FirebaseAuthConsumer>
-          {({ isSignedIn, user, providerId }) => {
-            return (
-              <pre style={{ height: 300, overflow: "auto" }}>
-                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
-              </pre>
-            );
-          }}
-        </FirebaseAuthConsumer>
-        <div>
-          <IfFirebaseAuthed>
-            {() => {
-              return <div>You are authenticated</div>;
-            }}
-          </IfFirebaseAuthed>
-          <IfFirebaseAuthedAnd
-            filter={({ providerId }) => providerId !== "anonymous"}
-          >
-            {({ providerId }) => {
-              return <div>You are authenticated with {providerId}</div>;
-            }}
-          </IfFirebaseAuthedAnd>
-        </div>
-      </div>
-    </FirebaseAuthProvider>
-  );
-};
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>, 
+  document.getElementById("root")
+);
