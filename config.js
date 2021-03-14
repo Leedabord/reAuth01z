b3d8122210424b9fc1216fb07708f633ef38c633
Lee <gwforumlakers@gmail.com>
2021-03-13 20:19:21 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const config = {
-  apiKey: "AIzaSyCU2riEO-VTzLdoB1SCWd64vcE6D-lYffM",
-  authDomain: "slashdev01.firebaseapp.com",
-  databaseURL: "https://slashdev01.firebaseio.com",
-  projectId: "slashdev01",
-  storageBucket: "slashdev01.appspot.com",
-  messagingSenderId: "606100177398",
-  appId: "1:606100177398:web:05d631067dca5b65fd47d7",
-  measurementId: "G-GR8R4S2ZVT"
+  apiKey: "",
+  authDomain: "<proj>.firebaseapp.com",
+  databaseURL: "https://<proj>.firebaseio.com",
+  projectId: "<proj>",
+  storageBucket: "<proj>.appspot.com",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
