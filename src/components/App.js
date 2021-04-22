5a22e619daebcaa4da428a07516eb6debe68971d
Leedabord <gwforumlakers@gmail.com>
2021-04-21 20:27:07 -0700

    1 file modified, 6 added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React from "react"
+import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
-// import Signup from "./Signup"
-// import Dashboard from "./Dashboard"
-// import Login from "./Login"
-// import PrivateRoute from "./PrivateRoute"
-// import ForgotPassword from "./ForgotPassword"
-// import UpdateProfile from "./UpdateProfile"
+import Dashboard from "./Dashboard"
+import Login from "./Login"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
